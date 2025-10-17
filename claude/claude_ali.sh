a0a50f218efc9161574e2bcd1fced3544234c84c
fub <jupiter.ben@live.com>
2025-10-17 10:20:59 +0800

    add

--- claude/claude_ali.sh
+++ claude/claude_ali.sh
@@ -1,4 +1,6 @@
 export ANTHROPIC_BASE_URL=https://dashscope.aliyuncs.com/api/v2/apps/claude-code-proxy
 export ANTHROPIC_AUTH_TOKEN=sk-2490ac0527454d3fb453728acd9cbd22
+export ANTHROPIC_MODEL=qwen3-coder-plus-2025-07-22
+export ANTHROPIC_SMALL_FAST_MODEL=qwen3-coder-plus
 
 claude
---
+++ claude/claude_bigmodel.sh
@@ -0,0 +1,5 @@
+export ANTHROPIC_BASE_URL="https://open.bigmodel.cn/api/anthropic"
+export ANTHROPIC_AUTH_TOKEN="5d1a07259f944264b8b15d5a0caeedbc.Mzsa15L5gSR1V9vd"
+export CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC=1
+
+claude
